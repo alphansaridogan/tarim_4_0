2485817ad0f152d066df29460e74baeb7a3c21a8
Mehmet DEMİR <mmt.demr42@gmail.com>
2022-05-08 22:01:02 +0300

    suggestionScreen üst kısım eklendi

--- lib/screens/suggestions_screen/suggestions_screen.dart
+++ lib/screens/suggestions_screen/suggestions_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
+import 'package:tarim_4_0/config/widget/drop_down_widget.dart';
+import 'package:tarim_4_0/constants/constants.dart';
 import 'package:tarim_4_0/screens/suggestions_screen/suggestions_screen_model.dart';
 
 class SuggestionsScreen extends StatelessWidget {
@@ -7,12 +9,64 @@ class SuggestionsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return ViewModelBuilder<SuggestionsScreenModel>.reactive(
       viewModelBuilder: () => SuggestionsScreenModel(),
       onModelReady: (model) => model.init(),
       builder: (context, model, child) => Scaffold(
-        appBar: AppBar(
-          title: const Text('Öneriler'),
+        backgroundColor: Constants.green,
+        body: Column(
+          children: [
+            Container(
+              height: size.height * 0.7,
+              width: size.width * 0.96,
+              decoration: const BoxDecoration(
+                color: Constants.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(150),
+                ),
+              ),
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 75,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        width: size.width * 0.45,
+                        child: Image.network(
+                          'https://www.cagri.com/Uploads/UrunResimleri/uzum-siyah-kg-cf9d.jpg',
+                        ),
+                      ),
+                      DropDownWidget(
+                          borderColor: Constants.white,
+                          value: model.product,
+                          width: 0.5,
+                          textStyle:
+                              Constants.headline1.apply(color: Constants.black),
+                          list: model.products,
+                          title: 'Ürün Seçiniz',
+                          onChanged: (value) {
+                            model.product = value;
+                          },
+                          size: size)
+                    ],
+                  ),
+                  Text(model.productDetail[0]
+                      [model.product]), //listedekine göre alanacak değer
+                ],
+              ),
+            ),
+            Row(
+              children: [
+                Column(
+                  children: [],
+                )
+              ],
+            )
+          ],
         ),
       ),
     );
--- lib/screens/suggestions_screen/suggestions_screen_model.dart
+++ lib/screens/suggestions_screen/suggestions_screen_model.dart
@@ -10,4 +10,19 @@ class SuggestionsScreenModel extends MainViewModel {
       settings: const RouteSettings(name: routeName),
     );
   }
+
+  String? product;
+  List<String> products = ['Üzüm', 'Elma'];
+  List productDetail = [
+    {
+      'Üzüm':
+          'Hava sıcaklıklarına bağlı olarak bu haftatoprak nemini %35 civarında tutulması önerilir.Küllenme hastalığı riskini önlemek için 15günde bir sıvı kükürt ile ilaçlama yapılmalıdır. İstenmeyen sürgünler budanmalı ana omcanın hava alması sağlanmalıdır.'
+    },
+    {
+      'Elma':
+          'Hava sıcaklıklarına bağlı olarak bu haftatoprak nemini %35 civarında tutulması önerilir.Küllenme hastalığı riskini önlemek için 15günde bir sıvı kükürt ile ilaçlama yapılmalıdır. İstenmeyen sürgünler budanmalı ana omcanın hava alması sağlanmalıdır.'
+    }
+  ];
+
+  bulma(String product) {}
 }
