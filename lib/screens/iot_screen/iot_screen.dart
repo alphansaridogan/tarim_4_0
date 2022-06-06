0fbecbfca5c8ac2f315f6ebe5b683924eb04b5bc
Mehmet DEMİR <mmt.demr42@gmail.com>
2022-06-06 15:22:57 +0300

    iot ekranı pixel hatası çözüldü

--- lib/screens/iot_screen/iot_screen.dart
+++ lib/screens/iot_screen/iot_screen.dart
@@ -13,95 +13,105 @@ class IotScreen extends StatelessWidget {
     return ViewModelBuilder<IotScreenModel>.reactive(
       viewModelBuilder: () => IotScreenModel(),
       onModelReady: (model) => model.init(),
-      builder: (context, model, child) => Scaffold(
-        backgroundColor: Constants.green,
-        body: Stack(
+      builder: (context, model, child) => SingleChildScrollView(
+        child: Column(
           children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Container(
-                  height: size.height * 0.25,
-                ),
-                Container(
-                  height: size.height * 0.66,
-                  decoration: const BoxDecoration(
-                    color: Constants.white,
-                    borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(0),
-                    ),
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(
-                      vertical: size.height * 0.12,
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
+            SizedBox(
+              height: size.height * 0.95,
+              child: Scaffold(
+                backgroundColor: Constants.green,
+                body: Stack(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: [
-                        Text(
-                          "Neden Tarım 4.0 Akıllı Sistemi Kullanmalıyız?",
-                          style:
-                              Constants.headline2.apply(color: Constants.black),
+                        Container(
+                          height: size.height * 0.25,
                         ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: model.text
-                              .map((e) => Padding(
-                                    padding: const EdgeInsets.only(
-                                        left: 20, top: 20),
-                                    child: Text(
-                                      "✅ $e",
-                                      style: Constants.subtitle1
-                                          .apply(color: Constants.black),
-                                    ),
-                                  ))
-                              .toList(),
+                        Container(
+                          height: size.height * 0.70,
+                          decoration: const BoxDecoration(
+                            color: Constants.white,
+                            borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(0),
+                            ),
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(
+                              vertical: size.height * 0.12,
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Neden Tarım 4.0 Akıllı Sistemi Kullanmalıyız?",
+                                  style: Constants.headline2
+                                      .apply(color: Constants.black),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: model.text
+                                      .map((e) => Padding(
+                                            padding: const EdgeInsets.only(
+                                                left: 20, top: 20),
+                                            child: Text(
+                                              "✅ $e",
+                                              style: Constants.subtitle1.apply(
+                                                  color: Constants.black),
+                                            ),
+                                          ))
+                                      .toList(),
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
                       ],
                     ),
-                  ),
-                ),
-              ],
-            ),
-            Positioned(
-              width: size.width,
-              top: 100,
-              child: Container(
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  boxShadow: [BoxShadow(
-                    blurRadius: 10,
-                    color: Colors.grey,
-                    spreadRadius: 4,
-                  )],
-                ),
-                child: CircleAvatar(
-                  backgroundColor: Constants.white,
-                  radius: 70,
-                  child: Image.asset("assets/logo.png",
-                  height: 80),
+                    Positioned(
+                      width: size.width,
+                      top: size.height * 0.15,
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          boxShadow: [
+                            BoxShadow(
+                              blurRadius: 10,
+                              color: Colors.grey,
+                              spreadRadius: 4,
+                            )
+                          ],
+                        ),
+                        child: CircleAvatar(
+                          backgroundColor: Constants.white,
+                          radius: 70,
+                          child: Image.asset("assets/logo.png", height: 80),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      width: size.width,
+                      bottom: size.height * 0.09,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: ButtonWidget(
+                          size: size,
+                          height: 0.06,
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/new_crop');
+                          },
+                          title: 'Satın Al',
+                          color: Constants.lightGreen,
+                        ),
+                      ),
+                    )
+                  ],
                 ),
               ),
             ),
-            Positioned(
-              width: size.width,
-              bottom: size.height * 0.03,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: ButtonWidget(
-                  size: size,
-                  height: 0.06,
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/new_crop');
-                  },
-                  title: 'Satın Al',
-                  color: Constants.lightGreen,
-                ),
-              ),
-            )
           ],
         ),
       ),
